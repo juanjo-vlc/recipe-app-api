579529795aa2444cd1d248564588e37ed7c0a486
Juanjo Garcia <juanjo@garciaamaya.com>
2023-06-12 22:17:23 +0200

    Added tests

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator funcions
+"""
+
+
+def add(x, y):
+    """Add x and y and return result."""
+    return x + y
+
+
+def substract(x, y):
+    """Substract x from y and return result."""
+    return y - x
