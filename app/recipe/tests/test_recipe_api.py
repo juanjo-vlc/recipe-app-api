448475085b931a6875bc02c9eeaf7cbe46c7b0c0
Juanjo Garcia <juanjo@garciaamaya.com>
2023-07-15 12:48:08 +0200

    Lesson 101

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -244,7 +244,7 @@ class PrivateRecipeApiTests(TestCase):
             self.assertTrue(exists)
 
     def test_create_tag_on_update(self):
-        """Test creating tag when updating a recipe."""
+        """Test create tag when updating a recipe."""
         recipe = create_recipe(user=self.user)
 
         payload = {'tags': [{'name': 'Lunch'}]}
@@ -258,7 +258,6 @@ class PrivateRecipeApiTests(TestCase):
     def test_update_recipe_assign_tag(self):
         """Test assigning an existing tag when updating recipe."""
         tag_breakfast = Tag.objects.create(user=self.user, name='Breakfast')
-
         recipe = create_recipe(user=self.user)
         recipe.tags.add(tag_breakfast)
 
@@ -277,9 +276,9 @@ class PrivateRecipeApiTests(TestCase):
         recipe = create_recipe(user=self.user)
         recipe.tags.add(tag)
 
-        payload = {'tags': {}}
+        payload = {'tags': []}
         url = detail_url(recipe.id)
         res = self.client.patch(url, payload, format='json')
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
-        self.assertEqual(res.tags.count(), 0)
+        self.assertEqual(recipe.tags.count(), 0)
