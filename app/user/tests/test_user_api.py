c0358c246d8bf05768c155b7e46fb5d891d918ba
Juanjo Garcia <juanjo@garciaamaya.com>
2023-07-15 13:09:26 +0200

    [skip ci] Lesson 101-3

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -99,7 +99,7 @@ class PublicUserApiTests(TestCase):
         payload = {'email': 'test@example.com', 'password': ''}
         res = self.client.post(TOKEN_URL, payload)
 
-        self.assertNotIn('token', 'res.data')
+        self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_retrieve_user_unauthorized(self):
